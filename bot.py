8a9f3f33e833481e608443134f4fd614fa80bdc3
CODER749-cell <birebirdestekicin@gmail.com>
2025-01-21 21:35:52 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+from config import token
+import random
+
+secenekler = [1 ,2 ,3 ,4 ,5]
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix='/', intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Giriş yaptı:  {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(client.command_prefix):
+        await client.process_commands(message)
+    else:
+        await message.channel.send(message.content)
+
+@client.command()
+async def about(ctx):
+    await ctx.send('Bu discord.py kütüphanesi ile oluşturulmuş echo-bot!')
+
+@client.command()
+async def choose(ctx):
+    await ctx.send(random.choose(secenekler[1]))
+
+client.run(token)
